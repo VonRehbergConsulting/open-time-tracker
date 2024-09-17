25ff8a9de6b95629ff8f35dc6e72af91f399d8a1
shtangey <denis.shtangey@gmail.com>
2024-09-17 10:27:35 +0200

    Fix comment suggestions layout

--- lib/modules/timer/ui/comment_suggestions_page.dart
+++ lib/modules/timer/ui/comment_suggestions_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:open_project_time_tracker/app/ui/widgets/list_item.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:open_project_time_tracker/app/ui/widgets/screens/scrollable_screen.dart';
 
 class CommentSuggestionsPage extends StatelessWidget {
   final List<String> comments;
@@ -11,23 +12,26 @@ class CommentSuggestionsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context).comment_suggestions_title),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(12.0),
-        children: comments
-            .map(
-              (comment) => ListItem(
-                title: comment,
-                subtitle: null,
-                comment: null,
-                trailing: null,
-                action: () => handler(comment),
-              ),
-            )
-            .toList(),
+    return SliverScreen(
+      title: AppLocalizations.of(context).comment_suggestions_title,
+      scrollingEnabled: true,
+      body: SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: comments
+                .map(
+                  (comment) => ListItem(
+                    title: comment,
+                    subtitle: null,
+                    comment: null,
+                    trailing: null,
+                    action: () => handler(comment),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
       ),
     );
   }
