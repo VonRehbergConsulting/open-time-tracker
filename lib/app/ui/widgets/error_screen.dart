bb28c4c422b53963ecbb309305406be467cd35d3
shtangey <denis.shtangey@gmail.com>
2024-09-24 10:36:42 +0200

    Fix error screen button

--- lib/app/ui/widgets/error_screen.dart
+++ lib/app/ui/widgets/error_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide FilledButton;
+import 'package:open_project_time_tracker/app/ui/widgets/filled_button.dart';
 
 class ErrorScreen extends StatelessWidget {
   final String text;
@@ -15,16 +15,26 @@ class ErrorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    final buttonPadding = deviceSize.width * 0.20;
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(text),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+            ),
             const SizedBox(height: 16.0),
-            CupertinoButton.filled(
-              onPressed: action,
-              child: Text(buttonText),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: buttonPadding),
+              child: FilledButton(
+                onPressed: action,
+                text: buttonText,
+              ),
             ),
           ],
         ),
